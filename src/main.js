0e7052bffabc23220c3ffd5d10599db145c707fa
KatherineLo <510175889@qq.com>
2025-04-16 18:08:21 +0800

    styles

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import './style.css'
 
 Vue.config.productionTip = false
 
